030e96003afc050262cb01e51e4dbe3ed3772695
jeongwongo <20123104a@gmail.com>
2024-05-06 21:37:52 +0900

    내 위치(현위치)로 이동하기
    -구글맵 컨트롤러에서 현위치 받아와서 이동시키기 => 에뮬레이터/시뮬레이터에 설정한 위치 ㅇㅇ

--- flutter_gps_prac/lib/screen/home_screen.dart
+++ flutter_gps_prac/lib/screen/home_screen.dart
@@ -18,6 +18,8 @@ class _HomeScreenState extends State<HomeScreen> {
     zoom: 15, //높을수록 확대
   );
 
+  late final GoogleMapController controller;
+
   checkPermission() async {
     //gps 확인
     final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
@@ -43,6 +45,21 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          '오늘도출근',
+          style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+              onPressed: myLocationPressed,
+              icon: const Icon(
+                Icons.my_location,
+                color: Colors.amber,
+              ))
+        ],
+      ),
       body: FutureBuilder(
         future: checkPermission(), //비동기함수
         builder: (context, snapshot) {
@@ -54,12 +71,30 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               Expanded(
                 child: GoogleMap(
+                  onMapCreated: (GoogleMapController controller) {
+                    this.controller = controller;
+                  },
                   initialCameraPosition: initialPosition,
+                  mapType: MapType.normal,
+                  myLocationEnabled: true, //현위치 표시
+                  myLocationButtonEnabled: false, //현위치가기 버튼
+                  zoomControlsEnabled: false,
                 ),
               )
             ],
           );
         },
+      ),
+    );
+  }
+
+  myLocationPressed() async {
+    //현위치 받아와서 이동하기
+    final location = await Geolocator.getCurrentPosition();
+
+    controller.animateCamera(
+      CameraUpdate.newLatLng(
+        LatLng(location.latitude, location.longitude),
       ),
     );
   }
